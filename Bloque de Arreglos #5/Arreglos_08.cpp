9e36a34859f36d13edf4bf58fa304f213d3bbc02
Luis Carlos Botero A <luboteroa@unal.edu.co>
2022-08-22 12:20:46 -0500

    Bloque de arreglos

--- Bloque de Arreglos #5/Arreglos_08.cpp
+++ Bloque de Arreglos #5/Arreglos_08.cpp
@@ -8,7 +8,24 @@ using namespace std;
 
 int main()
 {
+	int size; 
 	
+	cout<<"Introduzca el numero de numeros que desea introducir: "; cin>>size;
+	
+	int arregloUno[size], arregloDos[size];
+	
+	for(int i = 0; i<size; i++)
+	{
+		cout<<"Introduzca el numero en la posición "<<(i+1)<<": "; cin>>arregloUno[i]; 
+		
+		arregloDos[i] = arregloUno[i] * 2;
+	}
+	
+	cout<<"El arreglo dos es: ";
+	for(int i = 0; i<size; i++)
+	{
+		cout<<arregloDos[i]<<", ";
+	}
 	
 	getch();
 	return 0;
